56bce375e7c037e39f3ec64b8ae06e3fa14572fb
fabade29 <163082853+fabade29@users.noreply.github.com>
2025-05-01 15:19:19 -0300

    Update putty.js

--- putty.js
+++ putty.js
@@ -1,5 +1,22 @@
+// Detectar a própria pasta de execução
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var scriptPath = WScript.ScriptFullName;
+var dir = fso.GetParentFolderName(scriptPath);
+
+// Gerar nome aleatório para o putty.exe
+function randomName(len) {
+    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var name = "";
+    for (var i = 0; i < len; i++) {
+        name += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return name;
+}
+var exeName = randomName(8) + ".exe";
+var exePath = dir + "" + exeName;
+
+// Baixar e salvar o putty.exe na mesma pasta
 var url = "https://the.earth.li/~sgtatham/putty/latest/w64/putty.exe";
-var temp = WScript.CreateObject("WScript.Shell").ExpandEnvironmentStrings("%TEMP%") + "\putty.exe";
 var xHttp = new ActiveXObject("Microsoft.XMLHTTP");
 xHttp.open("GET", url, false);
 xHttp.send();
@@ -8,10 +25,10 @@ if (xHttp.status === 200) {
     stream.Type = 1;
     stream.Open();
     stream.Write(xHttp.responseBody);
-    stream.SaveToFile(temp, 2);
+    stream.SaveToFile(exePath, 2);
     stream.Close();
     var shell = new ActiveXObject("WScript.Shell");
-    shell.Run('"' + temp + '"', 1, false);
+    shell.Run('"' + exePath + '"', 1, false);
 } else {
     WScript.Echo("Erro ao baixar: " + xHttp.status);
 }
